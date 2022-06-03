de8baa3f9cfca4aaf12ffd77c7e8cefb72c56c02
JisuKim <jso_osj@naver.com>
2022-06-03 22:40:01 +0900

    :sparkle: LRU 알고리즘

--- jsAlgorithm/section7 - sort, greedy, 결정 알고리즘/section7-4.js
+++ jsAlgorithm/section7 - sort, greedy, 결정 알고리즘/section7-4.js
@@ -19,3 +19,20 @@ function selectionSort(numbers) {
 }
 
 console.log(selectionSort(numbers));
+
+function solution(size, arr) {
+  let answer = Array.from({length: size}, () => 0); //주어진 메모리 크기만큼 배열을 만들고 0으로 채운다.
+  arr.forEach(x => {
+    let pos = -1; //position
+    for (let i=0; i<size; i++) {
+      if (x ===answer[i]) pos = i; //메모리에 들어있는 작업 중 일치하는 것의 index를 찾는다.
+    }
+    if (pos === -1) {
+      //일치하는 작업이 없다면 miss
+      for (let i=size-1; i>=1; i--) {
+        answer[i] = answer[i-1];
+      }
+    }
+  })
+} 
+
